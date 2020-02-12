76b2596689fae412140eeec8b6e8306d32417cd3
Rushikesh Salunkhe <rushikeshsalunkhe101@gmail.com>
2020-02-12 17:08:34 +0530

    Promise rejection

--- api/LTS/controllers/tracking.js
+++ api/LTS/controllers/tracking.js
@@ -94,6 +94,7 @@ function totalDistance(tracking_id,newDistance){
               }
             ])
             .then(distanceTravelled=>{
+                console.log("distanceTravelled[0].distance",distanceTravelled[0]);
                 var totalDistanceTravelled = distanceTravelled[0].distance + newDistance;
                 console.log("totalDistanceTravelled",totalDistanceTravelled);
                 resolve(totalDistanceTravelled);
